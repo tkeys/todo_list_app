a1bd76f844ab1f1d49d10a086d292e626b1dd22f
Theokeys <theolarsenn@gmail.com>
2021-08-21 20:27:01 +0300

    testing the view and url

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def taskList(request):
+    return HttpResponse('This is a task list app')
 
 # Create your views here.
