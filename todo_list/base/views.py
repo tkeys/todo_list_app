7ec92fd2424b992923e895cae156678b2c67ea08
Theokeys <theolarsenn@gmail.com>
2021-09-02 19:23:21 +0300

    Search box finalized

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -36,7 +36,13 @@ class TaskList(LoginRequiredMixin, ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
-        context['count'] = context['tasks'].filter(complete=False).count
+        context['count'] = context['tasks'].filter(complete=False).count()
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(
+                title__icontains=search_input)
+            context[search_input] = search_input
         return context
 
 
